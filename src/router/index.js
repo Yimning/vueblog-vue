caf253ed2a68c49ea5ede7adfe07daae27cd6598
Yimning <1148967988@qq.com>
2020-11-09 09:26:44 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -53,3 +53,5 @@ const router = new VueRouter({
 })
 
 export default router
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-
     <el-container>
       <el-header>
-        <img class="mlogo" src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png" alt="">
+        <img
+          class="mlogo"
+          src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png"
+          alt=""
+        />
       </el-header>
       <el-main>
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
           <el-form-item label="用户名" prop="username">
             <el-input v-model="ruleForm.username"></el-input>
           </el-form-item>
@@ -15,114 +24,119 @@
           </el-form-item>
 
           <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >立即创建</el-button
+            >
             <el-button @click="resetForm('ruleForm')">重置</el-button>
           </el-form-item>
         </el-form>
-
       </el-main>
     </el-container>
-
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Login",
-    data() {
-      return {
-        ruleForm: {
-          username: 'markerhub',
-          password: '111111'
-        },
-        rules: {
-          username: [
-            { required: true, message: '请输入用户名', trigger: 'blur' },
-            { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
-          ],
-          password: [
-            { required: true, message: '请选择密码', trigger: 'change' }
-          ]
-        }
-      };
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            const that = this
-            this.$axios.post('/login', this.ruleForm).then(res => {
-          /*   本身的路由  http://localhost:8081/ 但是为了后续方便修改，一般放置于 axios.js中 */
-              console.log(res.data)
-              const jwt = res.headers['authorization']
-              const userInfo = res.data.data
-
-              // 把数据共享出去，存于this.store下
-              that.$store.commit("SET_TOKEN", jwt)
-              that.$store.commit("SET_USERINFO", userInfo)
-
-              // 获取
-              console.log(that.$store.getters.getUser)
-
-              that.$router.push("/blogs")   //跳转页
-            })
-
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
+export default {
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        username: "markerhub",
+        password: "111111",
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
-    }
-  }
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 15,
+            message: "长度在 3 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请选择密码", trigger: "change" },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const that = this;
+          this.$axios.post("/login", this.ruleForm).then((res) => {
+            /*   本身的路由  http://localhost:8081/ 但是为了后续方便修改，一般放置于 axios.js中 */
+            console.log(res.data);
+            //获取后台的authorization
+            const jwt = res.headers["authorization"];
+            const userInfo = res.data.data;
+
+            // 把数据共享出去，存于this.store下
+            that.$store.commit("SET_TOKEN", jwt);
+            that.$store.commit("SET_USERINFO", userInfo);
+
+            // 获取
+            console.log(that.$store.getters.getUser);
+            //跳转页
+            that.$router.push("/blogs");
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
 
-  .el-main {
-    /*background-color: #E9EEF3;*/
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
+.el-main {
+  /*background-color: #E9EEF3;*/
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
+body > .el-container {
+  margin-bottom: 40px;
+}
 
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
 
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 
-  .mlogo {
-    height: 60%;
-    margin-top: 10px;
-  }
+.mlogo {
+  height: 60%;
+  margin-top: 10px;
+}
 
-  .demo-ruleForm {
-    max-width: 500px;
-    margin: 0 auto;
-  }
-
+.demo-ruleForm {
+  max-width: 500px;
+  margin: 0 auto;
+}
 </style>
