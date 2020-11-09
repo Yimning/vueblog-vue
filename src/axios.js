454fc77339f9c655fa7b876aff780f886b0a1e25
Yimning <1148967988@qq.com>
2020-11-09 09:36:05 +0800

    Update

--- src/axios.js
+++ src/axios.js
@@ -25,7 +25,7 @@ axios.interceptors.response.use(response => {
       Element.Message.error('错了哦，这是一条错误消息', {duration: 3 * 1000})
 
       return Promise.reject(response.data.msg)
-    }
+    } 
   },
   error => {
     console.log(error)
